43ccff34846c884fa9c2c23ec6434fa9d4cd88df
Júlia Fonseca <olivia.ifrj@gmail.com>
2025-11-11 10:22:47 -0300

    update 3 - a)

--- exercicio 3 - a).jsx
+++ exercicio 3 - a).jsx
@@ -1,31 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+function ListaHobbies() {
+  const hobbies = ['Leitura', 'Desenhar', 'Programação', 'Culinária']
 
 
-
-function StatusJogo({nivel, pontos}){
-  const maxPontos = nivel * 1000
-  const progresso = Math.min((pontos / maxPontos) * 100, 100);
-  const corBarra = progresso < 30 ? 'red' : progresso < 70 ? 'orange' : 'green'
- 
-  return (
+  return(
     <div>
-      <h2>Nivel {nivel}</h2>
-      <p>Pontos: {pontos} / {maxPontos}</p>
-      <div className = "progress-container">
-        <div className = {`progress-bar ${corBarra}`} data-width = {progresso}></div>
-      </div>
-      <p>{progresso.toFixed(1)}% completo</p>
+      <h2>Meus Hobbies Favoritos</h2>
+      <ul>
+        {hobbies.map((hobby, index) => (
+         <li key = {index}>{hobby}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 
-function App(){
-  return <StatusJogo nivel = {5} pontos = {2000}/>
-}
-
-
-export default App
+export default ListaHobbies
