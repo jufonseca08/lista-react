c4afb932f40fb861228122ab42559faa86c577db
Júlia Fonseca <olivia.ifrj@gmail.com>
2025-11-11 10:45:39 -0300

    update 6 - b)

--- 6 - b).jsx
+++ 6 - b).jsx
@@ -1,31 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 
-
-function StatusJogo({nivel, pontos}){
-  const maxPontos = nivel * 1000
-  const progresso = Math.min((pontos / maxPontos) * 100, 100);
-  const corBarra = progresso < 30 ? 'red' : progresso < 70 ? 'orange' : 'green'
+function MostrarEsconder(){
+  const[mostrar, setMostrar] = useState(false);
  
-  return (
-    <div>
-      <h2>Nivel {nivel}</h2>
-      <p>Pontos: {pontos} / {maxPontos}</p>
-      <div className = "progress-container">
-        <div className = {`progress-bar ${corBarra}`} data-width = {progresso}></div>
-      </div>
-      <p>{progresso.toFixed(1)}% completo</p>
-    </div>
-  )
+  return(
+  <div>
+    <button onClick={() => setMostrar (!mostrar)}>
+      {mostrar ? 'Esconder foto' : 'Mostrar foto'}
+    </button>
+    {mostrar && <img src="src/assets/beyonce2.jpg"/>}
+  </div>
+
+
+  );
 }
 
 
-function App(){
-  return <StatusJogo nivel = {5} pontos = {2000}/>
-}
-
-
-export default App
+export default MostrarEsconder
